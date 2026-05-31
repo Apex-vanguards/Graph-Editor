6fd7f2a2269654421fc75ec5ad6637bb8662d5d7
Apex <mrcat19823@gmail.com>
2026-05-31 12:29:14 +0400

    Testing

--- src/main.cpp
+++ src/main.cpp
@@ -12,25 +12,21 @@ int main(int argc, char *argv[]) {
   Graph graph;
   Editor editor(graph);
 
-  // Test üçün 3 düyün yaradırıq
   editor.AddNode(1, {500, 200});
   editor.AddNode(2, {700, 300});
   editor.AddNode(3, {900, 200});
 
   while (!WindowShouldClose()) {
-    // İdarəetmə və Input
+
     editor.Update();
     editor.HandleUI();
     editor.HandleInput();
 
-    // Çəkmə (Rendering)
     BeginDrawing();
     ClearBackground(RAYWHITE);
 
-    // Qrafın elementləri
     editor.Draw();
 
-    // UI paneli ən üst qatda
     editor.DrawUI();
 
     EndDrawing();
